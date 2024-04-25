825d2731c93477ae7434eab12ba40a3bc72c200c
MikiyasMebrate <mikiyasmebrate@gmail.com>
2024-04-25 13:51:50 +0300

    Admin page updated

--- UserAdmin/urls.py
+++ UserAdmin/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='user-admin-index')
+    path('', views.index, name='user-admin-index'),
+    path('company/', views.company, name='user-admin-company'),
    ]
--- UserAdmin/views.py
+++ UserAdmin/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
+from Company.models import Company
 # Create your views here.
 def index(request):
     return render(request, 'UserAdmin/index.html')
+
+def company(request):
+    if request.method == "GET":
+        companies = Company.objects.all()
+        count = 30
+        paginator = Paginator(companies, 30) 
+        page_number = request.GET.get('page')
+    
+        try:
+            page = paginator.get_page(page_number)
+            try: count = (30 * (int(page_number) if page_number  else 1) ) - 30
+            except: count = (30 * (int(1) if page_number  else 1) ) - 30
+        except PageNotAnInteger:
+            # if page is not an integer, deliver the first page
+            page = paginator.page(1)
+            count = (30 * (int(1) if page_number  else 1) ) - 30
+        except EmptyPage:
+            # if the page is out of range, deliver the last page
+            page = paginator.page(paginator.num_pages)
+            count = (30 * (int(paginator.num_pages) if page_number  else 1) ) - 30
+        
+        print(page)
+        context = {
+            'companies' : page
+        }
+        return render(request, 'UserAdmin/company.html', context=context)
