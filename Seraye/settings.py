951ee2cd4eee2c34d8b977b3afbb416013d7b19f
mikiyasmebrate <mikiyasmebrate@gmail.com>
2024-07-06 13:25:23 +0300

    updated

--- Seraye/settings.py
+++ Seraye/settings.py
@@ -133,9 +133,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
+
 MEDIA_ROOT=os.path.join(BASE_DIR,"media/")
 MEDIA_URL='/media/'
-
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
@@ -143,10 +144,6 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
